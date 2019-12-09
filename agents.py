1f2ee97f45e3d6918ee5ebff6170abab65a7566f
Nando Davis <nandodavis@gmail.com>
2019-12-09 15:11:58 +0100

    Add switching lanes logic

--- agents.py
+++ agents.py
@@ -22,14 +22,13 @@ class Vehicle(Agent):
             self.last_acceleration = 0
             self.same_lane = 0
             self.move_vehicle(switch_lane)
+        if self.model.road_length <= self.pos[0] + self.model.space_between_vehicles + 1:
+            self.model.grid.remove_agent(self)
+            self.model.schedule.remove(self)
         else:
-            if self.model.road_length <= self.pos[0] + self.model.space_between_vehicles + 1:
-                self.model.grid.remove_agent(self)
-                self.model.schedule.remove(self)
-            else:
-                self.last_acceleration = space_in_front
-                self.same_lane += 1
-                self.move_vehicle((self.pos[0] + space_in_front, self.pos[1]))
+            self.last_acceleration = space_in_front
+            self.same_lane += 1
+            self.move_vehicle((self.pos[0] + space_in_front, self.pos[1]))
 
     def lane_switch(self, switch_space, space_in_front):
         # # 'pseudo' code to work out:
@@ -37,42 +36,52 @@ class Vehicle(Agent):
         #     direction = probably_down or stay_in_lane
         # if int(self.model.max_type_speed / 2) < self.max_vehicle_speed:
         #     direction = probably_up or stay_in_lane
+        space_below, location_below = False, False
+        space_above, location_above = False, False
 
         if self.pos[1] != 0:  # if lane is not bottom lane
             space_below, location_below = self.space_on_side(switch_space, -1)
-        else:
-            space_below, location_below = False, False
         if self.pos[1] < self.model.lanes - 1:  # if lane is not top lane
             space_above, location_above = self.space_on_side(switch_space, 1)
-        else:
-            space_above, location_above = False, False
 
         # // If statements to determine vehicle behaviour
         switch_lane = False
-        if space_in_front == 0 and space_below and self.last_acceleration == 0 and self.same_lane == 1:
-            switch_lane = location_below
-        elif space_in_front == 0 and space_above and self.last_acceleration == 0 and self.same_lane == 1:
-            switch_lane = location_above
-        elif space_in_front < self.max_vehicle_speed and self.same_lane == 5 and space_above:
-            switch_lane = location_above
-        elif space_below and space_in_front == self.max_vehicle_speed and self.same_lane == 5:
-            switch_lane = location_below
+        if space_in_front < self.max_vehicle_speed and space_below and space_above:
+            option = self.get_best_lane_switch
+            if option == 1:
+                return location_above
+            return location_below
+        elif space_in_front < self.max_vehicle_speed and space_below:
+            return location_below
+        elif space_in_front < self.max_vehicle_speed  and space_above:
+            return location_above
         return switch_lane
 
     def space_in_front(self):
+        return self.space_up_front(self.pos[0], self.pos[1])
+
+    def space_up_front(self, x, y):
         space = 0
-        for i in range(1, self.model.road_length - self.pos[0]):  # remaining cells until end road
-            if self.model.grid.is_cell_empty((self.pos[0] + i, self.pos[1])):
+        for i in range(1, self.model.road_length - x):  # remaining cells until end road
+            if self.model.grid.is_cell_empty((x + i, y)):
                 space += 1
+                if space >= self.max_vehicle_speed + self.model.space_between_vehicles:
+                    break
             else:
                 break
-        space = space - self.model.space_between_vehicles
-        if space > 0 and space > self.max_vehicle_speed:
-            return self.max_vehicle_speed
-        elif space > 0:
+        if space > 0:
             return space
+        return 0
+
+    def get_best_lane_switch(self):
+        space_above = self.space_up_front(self.pos[0], self.pos[1] + 1)
+        space_below = self.space_up_front(self.pos[0], self.pos[1] - 1)
+
+        if space_above > space_below:
+            return 1
         else:
             return 0
+
 
     def space_on_side(self, spacer, lane):
         switch_locations = []
