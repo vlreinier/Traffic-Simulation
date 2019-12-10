b893bb6d21c19d9d3044834ab68aecacde3a5f2f
vlreinier <43336873+vlreinier@users.noreply.github.com>
2019-12-10 14:19:18 +0100

    Vehicle behaviour

--- agents.py
+++ agents.py
@@ -14,13 +14,12 @@ class Vehicle(Agent):
 
     def advance(self):
         space_in_front = self.space_in_front()
-        space_on_front_and_back = floor(self.model.space_between_vehicles / 2)
+        space_on_front_and_back = int(self.model.space_between_vehicles / 2) + 1
         switch_lane = self.lane_switch(space_on_front_and_back, space_in_front)
         self.lane_forward(switch_lane, space_in_front)
 
     def lane_forward(self, switch_lane, space_in_front):
         if switch_lane:
-            self.last_acceleration = 0
             self.same_lane = 0
             self.move_vehicle(switch_lane)
         if self.model.road_length <= self.pos[0] + self.model.space_between_vehicles + 1:
@@ -29,7 +28,7 @@ class Vehicle(Agent):
             return
         self.last_acceleration = space_in_front
         self.same_lane += 1
-        if self.same_lane == 3: self.same_lane = 0
+        if self.same_lane == 5: self.same_lane = 0
         self.move_vehicle((self.pos[0] + space_in_front, self.pos[1]))
 
     def lane_switch(self, switch_space, space_in_front):
@@ -47,16 +46,19 @@ class Vehicle(Agent):
             space_above, location_above = self.space_on_side(switch_space, 1)
 
         # // If statements to determine vehicle behaviour
-        if space_in_front < self.max_vehicle_speed and space_below and space_above and self.same_lane == 0:
-            if self.get_best_lane_switch() == 0 and (floor(self.model.max_type_speed / 2) > self.max_vehicle_speed):
+        if space_in_front < self.max_vehicle_speed and space_below and space_above and \
+                (self.same_lane == 0 or (self.last_acceleration == 0 and self.same_lane % 2 == 0)):
+            if self.get_best_lane_switch() == 0 and (int(self.model.max_type_speed / 2) > self.max_vehicle_speed):
                 return location_above
             return location_below
-        elif space_in_front < self.max_vehicle_speed and space_below and self.same_lane == 0:
+        elif space_in_front < self.max_vehicle_speed and space_below and \
+                (self.same_lane == 0 or (self.last_acceleration == 0 and self.same_lane % 2 == 0)):
             return location_below
-        elif space_in_front < self.max_vehicle_speed  and space_above and self.same_lane == 0:
+        elif space_in_front < self.max_vehicle_speed  and space_above and \
+                (self.same_lane == 0 or (self.last_acceleration == 0 and self.same_lane % 2 == 0)):
             return location_above
         elif self.space_up_front(self.pos[0], self.pos[1] - 1) >= self.max_vehicle_speed and \
-                space_below and self.same_lane == 0:
+                space_below and (self.same_lane == 0 or (self.last_acceleration == 0 and self.same_lane % 2 == 0)):
             return location_below
         return False
 
